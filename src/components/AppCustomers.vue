5fe9dc295d7e6dfa5d5ec75a9f526a1b1b5f85a8
MladenGukic <mladengukic@gmail.com>
2019-11-22 20:54:11 +0100

    Implementirana forma za dodavanje novog kupca u listu

--- src/components/AppCustomers.vue
+++ src/components/AppCustomers.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="blog">
+      <form @submit.prevent="add()" class="form-control">
+          <input placeholder="Your Full Name" id="full-name" type="text" name="full-name" v-model="form.fullName">
+          <input placeholder="Your email" id="email" type="email" name="email" v-model="form.email">
+          <button type="submit">Add</button>
+      </form>  
       <h1> Customers </h1>
   <div v-bind:key="index" v-for="(customer, index) in customers">
       <ul>
@@ -17,13 +22,19 @@ import { customerService } from '../services/CustomerService'
 export default {
   data () {
     return {
-      customers: customerService.list()
+      customers: customerService.list(),
+
+        form: {fullName: '', email: '', products: [] }
     }
   },
 
   methods: {
       remove(customer) {
           customerService.remove(customer)
+      },
+
+      add() {
+          customerService.add(this.form)
       }
   },
 
